7a0cb2e2fb316bd56d890bedea105e4ea6bd1641
jchang4 <justinchangmusic@gmail.com>
2018-04-27 21:02:07 -0400

    Add CORS so frontend and backend can communicate

--- server/mealprep/mealprep.py
+++ server/mealprep/mealprep.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={'*': {'origins': 'http://localhost:3000'}}) # required for frontend
+
+# Set App Config
 app.config.from_object('config.app.DevelopmentConfig')
 # app.config.from_object('config.app.ProductionConfig')
 
