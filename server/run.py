21d8058e8115aa0cbb10636bc7bcc0ac6bf531dd
jchang4 <justinchangmusic@gmail.com>
2018-05-13 16:30:39 -0400

    Clean up run.py

--- server/run.py
+++ server/run.py
@@ -1,15 +1,7 @@
+from config.env import log_env, SERVICE_HOST, SERVICE_PORT
 from mealprep.mealprep import app
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    log_env()
 
-    # from mealprep.models import db, Profile
-    # from pprint import pprint
-
-    # pprint([p.to_dict() for p in Profile.query.all()])
-
-    # new_profile = Profile('Harry', 'harry@hogwarts.com')
-    # new_profile.last_name = 'Potter'
-    # s = db.session
-    # s.add(new_profile)
-    # s.commit()
+    app.run(host=SERVICE_HOST, port=SERVICE_PORT)
