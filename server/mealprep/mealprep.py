2b72cdf4874afe7c98d75da08dbd49592b9ce172
jchang4 <justinchangmusic@gmail.com>
2018-05-18 18:40:35 -0700

    Add minikube files and match cors to virtualbox ip to localhost

--- server/mealprep/mealprep.py
+++ server/mealprep/mealprep.py
@@ -2,11 +2,10 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={'*': {'origins': 'http://localhost:3000'}}) # required for frontend
+cors = CORS(app, resources={'*': {'origins': 'http://10.0.2.2:3000'}}) # required for frontend
 
 # Set App Config
 app.config.from_object('config.app.DevelopmentConfig')
-# app.config.from_object('config.app.ProductionConfig')
 
 from mealprep.models import db
 from mealprep.api import api
