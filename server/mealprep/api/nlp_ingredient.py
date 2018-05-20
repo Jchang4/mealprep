9c65b2f4ad758ece770048d00397c63f77d24a3b
jchang4 <justinchangmusic@gmail.com>
2018-05-19 21:45:43 -0700

    Update API to use reusable HTTP responses

--- server/mealprep/api/nlp_ingredient.py
+++ server/mealprep/api/nlp_ingredient.py
@@ -1,8 +1,9 @@
-from flask import request, jsonify
+from flask import request
 from flask_restful import Resource
-import requests
-from mealprep.api.helpers import classify_all_ingredients
-from mealprep.models import db, NLPIngredient
+from mealprep.mealprep import app
+from .helpers.responses import GenericSuccessResponse, CreatedNewItemResponse, BadRequest, ServerError
+from .helpers.ingredients import save_new_ingredient, already_classified_ingredient, classify_all_ingredients
+from .helpers.general import can_be_float
 
 
 class NLPIngredientApi(Resource):
@@ -10,38 +11,37 @@ class NLPIngredientApi(Resource):
         req_data = request.get_json(force=True, silent=True) or {}
 
         if self.has_required_data(req_data):
-            new_ingredient = NLPIngredient(original=(req_data['original'] or None),
-                                        name=(req_data['name'] or None),
-                                        quantity=(float(req_data.get('quantity'))\
-                                            if req_data.get('quantity')\
-                                            else None),
-                                        unit=(req_data.get('unit') or None),
-                                        comment=(req_data.get('comment') or None))
+            try:
+                already_classified_ingredient = already_classified_ingredient(req_data['original'], req_data['name'])
+                if already_classified_ingredient and not req_data.get('force'):
+                    return BadRequest("Ingredient has already been classified! If you want to save this again, use 'force'.")
 
-            # Add and save to database
-            db.session.add(new_ingredient)
-            db.session.commit()
+                new_ingredient = save_new_ingredient(req_data['original'],
+                                                     req_data['name'],
+                                                     req_data.get('quantity'),
+                                                     req_data.get('unit'),
+                                                     req_data.get('comment'))
 
-            return jsonify({
-                'status': 200,
-                'data': new_ingredient.to_dict(),
-            })
+                return CreatedNewItemResponse(data=new_ingredient.to_dict())
+
+            except Exception as e:
+                app.logger.error(repr(e))
+                return ServerError('Server Error. Postgres failed to add new nlp ingredient.')
+
         else:
-            return jsonify({
-                'status': 400,
-                'error': 'Bad request. Missing required parameters or bad parameters.',
-            })
+            return BadRequest('Missing required parameters or bad parameters.')
 
 
     def has_required_data(self, data):
+        """ Requirements:
+                (required) original
+                (required) name
+                (optional) quantity : must be a float
+        """
         if not data.get('original') or not data.get('name'):
             return False
-        if data.get('quantity'): # Ensure quantity can be a float
-            quant = data['quantity']
-            try:
-                float(quant)
-            except:
-                return False
+        if data.get('quantity') and not can_be_float(data['quantity']):
+            return False
         return True
 
 
@@ -51,18 +51,9 @@ class ClassifyIngredientApi(Resource):
         if data.get('ingredients'):
             try:
                 ingr = classify_all_ingredients(data['ingredients'])
-                return jsonify({
-                    'status': 200,
-                    'data': ingr,
-                })
+                return GenericSuccessResponse(data=ingr)
             except Exception as e:
-                print(e)
-                return jsonify({
-                    'status': 500,
-                    'error': 'Something went wrong!',
-                })
-
-        return jsonify({
-            'status': 400,
-            'error': 'Bad request. Missing params.',
-        })
+                app.logger.error(repr(e))
+                return ServerError('Something went wrong when trying to classify ingredients')
+        else:
+            return BadRequest('Missing parameters: ingredients')
--- server/mealprep/api/recipe.py
+++ server/mealprep/api/recipe.py
@@ -1,6 +1,8 @@
-import mealprep.food_services.food2fork as Food2Fork
 from flask import request, jsonify
 from flask_restful import Resource
+from mealprep.mealprep import app
+import mealprep.food_services.food2fork as Food2Fork
+from .helpers.responses import GenericSuccessResponse, BadRequest, NotFound, ServerError
 
 class GetRecipesApi(Resource):
     def post(self):
@@ -8,38 +10,25 @@ class GetRecipesApi(Resource):
         query = data.get('query')
         sort = data.get('sort') or 'r'
         page = data.get('page') or '1'
-
         if query:
             try:
                 recipes = Food2Fork.search(query, sort=sort, page=page)
                 recipes['recipes'].sort(key=lambda r: r['social_rank'], reverse=True) # Descending
-                return jsonify({
-                    'status': 200,
-                    'count': recipes['count'],
-                    'data': recipes['recipes'],
-                })
-            except:
-                return jsonify({
-                    'status': 500,
-                    'error': 'May day, may day! Food2Fork is down!',
-                })
+                return GenericSuccessResponse(count=recipes['count'], data=recipes['recipes'])
+            except Exception as e:
+                app.logger.error(repr(e))
+                return ServerError('May day, may day! Food2Fork is down!')
         else:
-            return jsonify({
-                'status': 400,
-                'error': 'Bad request. Check your query.',
-            })
+            return BadRequest('Check your query.')
 
 
 class GetRecipeByIdApi(Resource):
     def get(self, recipe_id):
-        r = Food2Fork.get_recipe(recipe_id)
-        if r:
-            return jsonify({
-                'status': 200,
-                'data': r['recipe'],
-            })
-
-        return jsonify({
-            'status': 400,
-            'error': 'Could not find ',
-        })
+        try:
+            r = Food2Fork.get_recipe(recipe_id)
+            if r:
+                return GenericSuccessResponse(data=r)
+            return NotFound('Could not find recipe with id: {}'.format(recipe_id))
+        except Exception as e:
+            app.logger.error(repr(e))
+            return ServerError('May day, may day! Food2Fork is down!')
