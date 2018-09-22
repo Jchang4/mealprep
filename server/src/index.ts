126b2c2047a4f36c8839a48868123e745b93863b
jchang4 <justinchangmusic@gmail.com>
2018-09-21 18:32:47 -0700

    Setup typescript development

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,12 @@
 import express from 'express'
-
-// Prettify Things
 import figlet from 'figlet'
 
-// Get Config
-import config from './config'
+const app = express();
 
-const app = express()
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(config.PORT, () => {
-    console.log(`Example app listening on port ${port}!`)
-    console.log(figlet('Meal Prep Server'))
-})
+app.listen(3000, () => {
+    console.log(figlet.textSync('Mealprep App'))
+});
