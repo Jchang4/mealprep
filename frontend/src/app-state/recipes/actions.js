fb718ee57752d6733d4649c59602004ea367a837
jchang4 <justinchangmusic@gmail.com>
2019-01-24 23:00:39 -0800

    More cleanup, but things are working well

--- frontend/src/app-state/recipes/actions.js
+++ frontend/src/app-state/recipes/actions.js
@@ -1,3 +1,4 @@
+import delay from "delay";
 import { getRecipesByIngredientsFromApi } from "api";
 
 import {
@@ -10,12 +11,15 @@ import {
 
 export function getRecipesByIngredients(ingredients, numResults = 7) {
   return async (dispatch, _) => {
-    const recipes = await getRecipesByIngredientsFromApi({
-      ingredients,
-      numResults,
-      offset: 0
-    });
-    dispatch({ type: ADD_N_RECIPES, payload: recipes });
+    for (let i = 0; i < numResults; i += 4) {
+      const recipes = await getRecipesByIngredientsFromApi({
+        ingredients,
+        numResults: 4,
+        offset: i
+      });
+      await delay(1000);
+      dispatch({ type: ADD_N_RECIPES, payload: recipes });
+    }
   };
 }
 
